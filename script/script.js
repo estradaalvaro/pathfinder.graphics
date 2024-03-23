d3f51b0490a3d54c466b51e7a408463da1da0557
alvi <alvaroe1992@yahoo.com>
2024-03-23 15:33:57 +1100

    added alert for send button

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
 function sendEmail() {
     window.location.href = "mailto:mirandaclark364@gmail.com";
+    alert("Thank you! Your message has been sent, and we will be in touch with you soon.");
   }
