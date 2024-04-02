9211c322163bb80e87fb8202b0d3d9878a8f1732
alvi <alvaroe1992@yahoo.com>
2024-04-02 12:46:00 +1100

    aprils update

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 function sendEmail() {
     window.location.href = "mailto:mirandaclark364@gmail.com";
     alert("Thank you! Your message has been sent, and we will be in touch with you soon.");
-  }
+  };
+
